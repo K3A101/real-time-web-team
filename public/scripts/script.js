6f824fa32058189c3cd6ddc0aa0e6fd0b3cb0c4d
K3A101 <keisha.alexander@hva.nl>
2023-04-19 09:00:58 +0200

    speech bubble is  gefixt

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -24,10 +24,17 @@ submit.addEventListener('click', event => {
 socket.on('message', message => {
     const li_element = document.createElement('li');
     li_element.textContent = ` ${message.username} : ${message.message} `;
-    // li_element.classList.add('talk-bubble',' tri-right' ,'border' ,'round btm-left-in');
+    
+  
     li_element.setAttribute('class', 'talk-bubble tri-right border round btm-left-in');
     messages.appendChild(li_element);
     messages.scrollTop = messages.scrollHeight;
+  
+    if (message.username === usernameInput.value) {
+        li_element.classList.add('message');
+    }
+
+
 })
 
 function insertDate() {
